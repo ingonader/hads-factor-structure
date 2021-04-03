d41a4d4b74fab6bb4a1770ba87915670e7a98828
Ingo Nader <ingo.nader@gmail.com>
2021-04-03 21:20:55 +0200

    Add documentation

--- R/function-library.R
+++ R/function-library.R
@@ -2,6 +2,22 @@
 ## Function definitions
 ## ######################################################################### ##
 
+
+#' Check value range (min/max) for continuous variables
+#' 
+#' Check how many values of a given data.frame with continuous variables 
+#' are below the minimum or above the maximum of the correct value range.
+#'
+#' @param df A data.frame with the relevant variables, which all need
+#'   to be continuous.
+#' @param val_rng The value range: minimum and maximum of acceptable
+#'   values.
+#'
+#' @return A vector with length \code{nrow(df)}, containing the number 
+#'   of violations for each row of the given data.frame.
+#' @export
+#'
+#' @examples
 check_minmax <- function(df, val_rng) {
   min_val <- min(val_rng)
   max_val <- max(val_rng)
@@ -11,6 +27,22 @@ check_minmax <- function(df, val_rng) {
   return(n_below_min + n_above_max)
 }
 
+
+#' Check value range for categorical variables
+#' 
+#' Check how many values of a given data.frame with categorical or discrete 
+#' variables have values that are not in the allowed set of values.
+#'
+#' @param df A data.frame with the relevant variables, which all need
+#'   to be discrete.
+#' @param val_rng A vector with allowed values. Missings (NA) are not
+#'   counted as violations.
+#'
+#' @return A vector with length \code{nrow(df)}, containing the number 
+#'   of violations for each row of the given data.frame.
+#' @export
+#'
+#' @examples
 check_valrange <- function(df, val_rng) {
   ## check value range:
   n_not_valrng <- apply(df, 1:2, function(i)
