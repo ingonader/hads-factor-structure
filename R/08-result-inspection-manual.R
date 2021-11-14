87268e841695dba29f7af9a510d1ef7a81d42aa1
Ingo Nader <ingo.nader@gmail.com>
2021-11-14 18:56:22 +0100

    Update data dump

--- R/08-result-inspection-manual.R
+++ R/08-result-inspection-manual.R
@@ -25,7 +25,7 @@ library(lavaan)
 ## load data dump from disk
 ## ========================================================================= ##
 
-# load(file = file.path(path_tmp, "results-all_2021-11-10--17-06-22---e9f84c44.Rdata"))
+# load(file = file.path(path_tmp, "results-all_2021-11-14--18-55-07---822dcf8e.Rdata"))
 
 ## ========================================================================= ##
 ## CFA models
